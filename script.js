616fc609d97e9d2b8eaceb4f3d753d6a580621dd
Langdon Douglas <langdon.douglas26@gmail.com>
2024-09-10 12:56:17 -0500

    Added Operate function

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-let a = 30;
+let num1 = 15;
+let num2 = 0;
+let operator = "+";
+
+
+let a = 0;
 let b = 0; 
 
 function add(a, b) {
@@ -14,14 +19,29 @@ function times(a, b) {
 } 
 
 function divide(a, b) {
-    if(a == 0 || b == 0){
+    if(a === 0 || b === 0){
         alert("ERROR: Can not use 0 to divide");
     }
     return a / b; 
 }
 
-test = divide(a, b);
-console.log(test);
+function operate(a, op, b) {
+    num1 = a; 
+    num2 = b; 
+    sum = "";
+
+    if(op === "+") {
+        return add(num1, num2);
+    }else if(op === "-") {
+        return sub(num1, num2);
+    }else if (op === "*"){
+        return times(num1, num2);
+    }else if (op === "/"){
+        return divide(num1, num2);
+    }
+
+}
 
 
-
+let result = operate(num1, "/", num2);
+console.log(result);
