6c8690569661ba8f0a762c556cae88c3d5afa35f
snooze26 <langdon.douglas26@gmail.com>
2024-09-10 18:58:42 -0500

    fixed equalClick function / updated psuedo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//variables
 const screenText = document.querySelector("#screen");
 const numberBtn = document.querySelectorAll("#num-btn");
 const operatorBtn = document.querySelectorAll("#op-btn");
@@ -10,7 +10,7 @@ let num2 = null;
 let operator = null;
 let currentInput = "";
 
-
+//functions 
 function add(a, b) {
     return a + b;
 }
@@ -34,7 +34,6 @@ function divide(a, b) {
 function operate(a, op, b) {
     num1 = a; 
     num2 = b; 
-    sum = "";
 
     if(op === "+") {
         return add(num1, num2);
@@ -47,8 +46,7 @@ function operate(a, op, b) {
     }
 }
 
-//capturing value to populate screen text
-function numberClick(event){
+function numberClick(event) {
 
     const number = event.target.textContent;
     currentInput += number;
@@ -56,47 +54,46 @@ function numberClick(event){
 
 } 
 
-function operatorClick(event){
+function operatorClick(event) {
     if(currentInput === "") return; 
 
     if(num1 === null){
-        num1 = parseFloat(currentInput);
-        operator = event.target.textContent;
-        currentInput = "";
-        screenText.textContent = operator;
-    }else if(num2 === null && currentInput !== ""){
-        num2 = parseFloat(currentInput);
-        const result = operate(num1, operator, num2);
-        screenText.textContent = result;
-        num1 = result;
-        num2 = null;
-        operator = event.target.textContent;
-        currentInput = "";
+        num1 = parseFloat(currentInput); 
+        operator = event.target.textContent; 
+        currentInput = ""; 
+        screenText.textContent = operator; 
 
+    }else if(num2 === null && currentInput !== ""){
+        num2 = parseFloat(currentInput); 
+        const result = operate(num1, operator, num2);
+        screenText.textContent = result; 
+        num1 = result; 
+        num2 = null; 
+        operator = event.target.textContent; 
+        currentInput = ""; 
     }
 }
 
-function equalClick(){
+function equalClick() {
     if(num1 !== null && operator !== null && currentInput !== ""){
         num2 = parseFloat(currentInput);
         const result = operate(num1, operator, num2);
         screenText.textContent = result;
-        num1 = result;
+        num1 = null;
         num2 = null;
         operator = null;
-        currentInput = "";
+        currentInput = result;
+
     }
 }
 
 // event listeners
 for(const number of numberBtn) {
     const value = number.addEventListener("click", numberClick);
-    console.log(number);
 }
 
 for(const op of operatorBtn) {
     const value = op.addEventListener("click", operatorClick);
-    console.log(value);
 }
 
 equalBtn.addEventListener("click", equalClick);
